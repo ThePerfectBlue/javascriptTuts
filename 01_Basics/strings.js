adaeed3557f0c4e8d9abed708c617e5221c09756
Ravi Kumar <77407433+ThePerfectBlue@users.noreply.github.com>
2024-03-16 11:09:48 +0000

    Strings

---
+++ 01_Basics/strings.js
@@ -0,0 +1,47 @@
+// string interpolation using ``
+    var name = "RK"
+
+    console.log(`hELLO MY NAMe is ${name}`);
+
+    const name2 = new String('smaug')
+
+    console.log(name2[1]);
+
+    const name3 = "smaug"
+const slice = name3.slice(1,3)
+console.log(slice)
+
+at()
+charAt()
+charCodeAt()
+codePointAt()
+concat()
+endsWith()
+fromCharCode()
+includes()
+indexOf()
+lastIndexOf()
+length
+localeCompare()
+match()
+padEnd()
+padStart()
+prototype
+repeat()
+replace()
+replaceAll()
+search()
+slice()
+split()
+startsWith()
+substr()
+substring()
+toLocaleLowerCase()
+toLocaleUpperCase()
+toLowerCase()
+toString()
+toUpperCase()
+trim()
+trimEnd()
+trimStart()
+valueOf()
